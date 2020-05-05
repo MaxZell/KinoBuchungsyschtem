8d82eea86c3ffdc44b39546e28ab222a197f1237
mzelensky <max.m.zelensky@gmail.com>
2020-05-05 15:08:15 +0200

    ver 1

---
+++ src/Date.java
@@ -0,0 +1,9 @@
+////personal data type for film showing date/time
+//
+//public class Date {
+//    public final String[] date;
+//
+//    public Date(String[] date) {
+//        this.date = date;
+//    }
+//}
